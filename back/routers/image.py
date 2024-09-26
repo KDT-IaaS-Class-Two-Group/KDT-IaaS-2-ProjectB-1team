ec375d235997c1d58323c51fb8f164e3b711d7b5
moonhr <moonhr153@gmail.com>
2024-09-26 11:40:20 +0900

    Feat | image 라우터
    /process-image엔드포인트 post 시
    이미지 전처리 과정을 거치고
    pil로 변환

--- back/routers/image.py
+++ back/routers/image.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
-from fastapi.responses import FileResponse
-from utils.file_operations import save_image, get_image_path
-import os #표준 라이브러리 중 하나로, 운영체제 관련 기능
+import numpy as np
+from utils.file_operations import save_image
+import io
+from fastapi.responses import StreamingResponse
 
 router = APIRouter()
 
@@ -11,10 +12,27 @@ async def upload_image(file: UploadFile = File(...)):  #UploadFile은 FastAPI에
     file_location = save_image(file)  # 이미지를 저장하는 함수 호출
     return {"img": file, "text": file_location}
 
-# # 이미지 다운로드 엔드포인트
-# @router.get("/download/{image_id}")
-# async def download_image(image_id: str):
-#     file_path = get_image_path(image_id)  # 이미지 경로를 가져오는 함수 호출
-#     if not os.path.isfile(file_path):
-#         raise HTTPException(status_code=404, detail="Image not found")
-#     return FileResponse(file_path)
+from service.image_preprocessing import process_image,convert_cv_to_pil
+@router.post("/process-image")
+async def process_img(image: UploadFile = File(...)):
+    try:
+        print("이미지 입력")
+        img_data = await image.read()
+        process_img = process_image(img_data, 300)
+        if len(process_img) > 0:
+            # 첫 번째 얼굴 ROI를 사용하여 결과를 반환
+            img_pil = convert_cv_to_pil((process_img[0] * 255.0).astype(np.uint8))
+        else:
+            # 얼굴이 발견되지 않은 경우 에러 처리
+            raise HTTPException(status_code=404, detail="No faces detected.")
+        
+        # 이미지를 바이트스트림으로 변환
+        byte_io = io.BytesIO()
+        img_pil.save(byte_io, format="PNG")
+        byte_io.seek(0)
+
+        return StreamingResponse(byte_io, media_type="image/png")
+        
+    except Exception as e:
+        # 예외 발생 시 에러 메시지 반환
+        return {"error": str(e)}, 400  # HTTP 400 Bad Request
