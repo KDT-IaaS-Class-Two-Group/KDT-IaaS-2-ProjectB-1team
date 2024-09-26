ba479b8c0883cbc11e0be3c9e9349be2d6b59d40
신지윤 <ranji12@naver.com>
2024-09-26 10:36:56 +0900

    Fix | .json파일 안에 내용이 비어있을 때 오류 수정

--- opencv/test.py
+++ opencv/test.py
@@ -68,7 +68,6 @@ def process_image(image_path):
         
     # 원하는 저장 폴더 경로 설정
     json_folder = os.path.join(os.getcwd(), 'json')
-
     os.makedirs(json_folder, exist_ok=True)  # 폴더가 없으면 생성
 
     # JSON 파일 이름 설정
@@ -84,8 +83,8 @@ def process_image(image_path):
     counter = 1  # 중복 파일을 위한 카운터 초기화
     
     # 이미지 파일 이름을 키로 사용하여 새로운 데이터 추가
+    key = os.path.basename(image_path)
     if status == "success":
-        key = os.path.basename(image_path)
         while key in existing_data:
             counter += 1
             key = f"{counter}{os.path.basename(image_path)}"
@@ -102,7 +101,6 @@ def process_image(image_path):
         desired_folder = os.path.join(os.getcwd(), 'successFolder/successImg')
     else:
         desired_folder = os.path.join(os.getcwd(), 'failFolder/failImg')
-
     os.makedirs(desired_folder, exist_ok=True)  # 폴더가 없으면 생성
 
 
@@ -123,9 +121,20 @@ def open_file():
         
 # integration.json 파일 불러오기
 def load_json_data():
-    with open('json/integration.json', 'r') as file:
-        data = json.load(file)
-    return data
+    try:
+        with open('json/integration.json', 'r') as file:
+            # 파일이 비어 있으면 빈 딕셔너리를 반환
+            if os.stat('json/integration.json').st_size == 0:
+                print("Warning: JSON file is empty.")
+                return {}
+            data = json.load(file)
+        return data
+    except json.JSONDecodeError:
+        print(f"Error: JSON decoding failed for {'json/integration.json'}. File may be corrupted.")
+        return {}
+    except Exception as e:
+        print(f"Error: {e}")
+        return {}
 
 # 객체가 비어있는지 확인하는 함수
 def check_success(obj):
@@ -143,12 +152,18 @@ def update_treeview(data):
 # 파일 변경 감지 및 업데이트 함수
 def check_for_updates():
     global last_modified_time
+    
+    if not os.path.exists('json/integration.json'):
+        print(f"Error: {'json/integration.json'} does not exist.")
+        return
+    
     current_modified_time = os.path.getmtime('json/integration.json')  # 파일의 마지막 수정 시간 확인
     
     if current_modified_time != last_modified_time:
         last_modified_time = current_modified_time
         data = load_json_data()
-        update_treeview(data)
+        if data is not None:
+            update_treeview(data)
     
     # 1000ms(1초) 후에 다시 check_for_updates 함수 실행
     root.after(1000, check_for_updates)
@@ -178,8 +193,11 @@ tree.pack(fill=tk.BOTH, expand=True)
 data = load_json_data()
 update_treeview(data)
 
-# 파일의 마지막 수정 시간 저장
-last_modified_time = os.path.getmtime('json/integration.json')
+# 프로그램이 시작할 때 JSON 파일의 존재 여부를 확인하고 초기화
+if os.path.exists('json/integration.json'):
+    last_modified_time = os.path.getmtime('json/integration.json')
+else:
+    last_modified_time = None  # 파일이 없는 경우 초기화
 
 # 주기적으로 파일 변경 감지
 root.after(1000, check_for_updates)  # 1000ms(1초)마다 파일 업데이트 확인
